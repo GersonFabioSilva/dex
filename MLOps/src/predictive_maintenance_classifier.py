9a6765a01f3ec626881379b38ca0fd1c22dedff2
GersonFabioSilva <gerson.fab@gmail.com>
2024-11-19 18:49:10 -0300

    entrypoint.py done

---
+++ MLOps/src/predictive_maintenance_classifier.py
@@ -0,0 +1,27 @@
+
+import tensorflow as tf
+import numpy as np
+import joblib
+from tensorflow.keras.models import load_model
+
+model = load_model("../models/pm_classificacao.h5")
+
+label_encoder = joblib.load("../models/label_encoder.pkl")
+scaler = joblib.load("../models/scaler.pkl")
+
+def predict(data: dict):
+
+    new_sample = np.array(list(data.values()))
+    new_sample_scaled = scaler.transform(new_sample)
+
+    predictions = model.predict(new_sample_scaled)[0]
+
+    probabilities = {'Heat Dissipation'     : predictions[0],
+                     'No Failure'           : predictions[1],
+                     'Overstrain Failure'   : predictions[2],
+                     'Power Failure'        : predictions[3],
+                     'Random Failures'      : predictions[4],
+                     'Tool Wear Failure'    : predictions[5]
+                     }   
+
+    return probabilities
